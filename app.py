880ef3509b28a0dd110ca00504e60159ce469f2c
William Mak <william@wmak.io>
2020-09-01 14:27:12 -0400

    Add enpdoints that create trends

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
+from __future__ import absolute_import
 import sentry_sdk
+import logging
+import time
+import datetime
+import random
+import math
 from flask import Flask
 from sentry_sdk.integrations.flask import FlaskIntegration
 
-sentry_sdk.init(
-    dsn="http://f003c07d43a347eb8f42eb93cf1e83e4@dev.getsentry.net:8000/8",
+logger = logging.getLogger(__name__)
 
-    environment="this environment",
-    integrations=[FlaskIntegration()]
+sentry_sdk.init(
+    dsn="http://3dccbba1a9fb42b98d916994e346438e@dev.getsentry.net:8000/9",
+    environment="slow",
+    integrations=[FlaskIntegration()],
+    traces_sample_rate=1.0,
 )
+start = datetime.datetime(2020, 8, 11, 18, 6, 56)
 
 app = Flask(__name__)
 
@@ -16,7 +25,81 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/query', methods=['POST'])
+def query():
+    1/0
 
-@app.route('/debug-sentry')
-def trigger_error():
-    division_by_zero = 1 / 0
+
+@app.route('/just_noise2')
+def just_noise2():
+    with sentry_sdk.configure_scope() as scope:
+        import time
+        time.sleep(random.random())
+        scope.user = {'id': random.randint(1,5), "email": random.choice([
+            u"foos@example.\u4e86\0\0\0\0",
+            u"bar@example.\u4e86\0\0\0\0",
+        ])}
+    return 'hi'
+
+@app.route('/just_noise')
+def just_noise():
+    with sentry_sdk.configure_scope() as scope:
+        import time
+        time.sleep(random.random())
+        scope.user = {'id': random.randint(1,5), "email": random.choice([
+            u"foos@example.\u4e86\0\0\0\0",
+            u"bar@example.\u4e86\0\0\0\0",
+        ])}
+    return 'hi'
+
+@app.route('/one_step')
+def one_step():
+    with sentry_sdk.configure_scope() as scope:
+        import time
+        hour = 1 if (datetime.datetime.now() - start).total_seconds()/3600 > 6 else 0
+        time.sleep(random.random() + hour)
+        scope.user = {'id': random.randint(1,5), "email": random.choice([
+            u"foos@example.\u4e86\0\0\0\0",
+            u"bar@example.\u4e86\0\0\0\0",
+        ])}
+    return 'hi'
+
+@app.route('/multiple_step')
+def multiple_step():
+    with sentry_sdk.configure_scope() as scope:
+        import time
+        hour = math.floor((datetime.datetime.now() - start).total_seconds()/7200.0)
+        time.sleep(random.random() + hour)
+        scope.user = {'id': random.randint(1,5), "email": random.choice([
+            u"foos@example.\u4e86\0\0\0\0",
+            u"bar@example.\u4e86\0\0\0\0",
+        ])}
+    return 'hi'
+
+@app.route('/linear')
+def linear():
+    with sentry_sdk.configure_scope() as scope:
+        import time
+        hour = (datetime.datetime.now() - start).total_seconds()/7200.0
+        time.sleep(random.random() + hour)
+        scope.user = {'id': random.randint(1,5), "email": random.choice([
+            u"foos@example.\u4e86\0\0\0\0",
+            u"bar@example.\u4e86\0\0\0\0",
+        ])}
+    return 'hi'
+
+@app.route('/spike')
+def spike():
+    with sentry_sdk.configure_scope() as scope:
+        import time
+        hour = 3 if datetime.datetime.now().hour == 7 else 0
+        time.sleep(random.random() + hour)
+        scope.user = {'id': random.randint(1,5), "email": random.choice([
+            u"foos@example.\u4e86\0\0\0\0",
+            u"bar@example.\u4e86\0\0\0\0",
+        ])}
+    return 'hi'
+
+
+def trigger_inner_error():
+    1 / 0
